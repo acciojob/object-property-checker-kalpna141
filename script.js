87e13397852a373ec1b365fb10892d9591c89425
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-14 15:31:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	 if(key==sampleObject.key)
+	 return "true";
+	else
+	return "false";
+ 
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// // Do not change the code below
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
